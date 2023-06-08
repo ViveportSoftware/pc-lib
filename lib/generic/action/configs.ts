c9fdd92753e37cc7c19469ca14d0adbeb019662b
Ahdai Tsai <ahdai_tsai@htc.com>
2023-06-08 15:38:18 +0800

    Add script attribute configs for script trigger and action

---
+++ lib/generic/action/configs.ts
@@ -0,0 +1,26 @@
+export default {
+  type: 'json',
+  schema: [
+    {
+      name: 'type',
+      type: 'number',
+      default: 0,
+      description: 'action type',
+    },
+    {
+      name: 'booleanParams',
+      type: 'boolean',
+      array: true,
+    },
+    {
+      name: 'stringParams',
+      type: 'string',
+      array: true,
+    },
+    {
+      name: 'numberParams',
+      type: 'number',
+      array: true,
+    },
+  ],
+};
---
+++ lib/generic/trigger/configs.ts
@@ -0,0 +1,110 @@
+export default {
+  type: 'json',
+  schema: [
+    {
+      name: 'type',
+      type: 'number',
+      default: 0,
+      description: 'trigger type',
+    },
+    {
+      name: 'booleanParams',
+      type: 'boolean',
+      array: true,
+    },
+    {
+      name: 'stringParams',
+      type: 'string',
+      array: true,
+    },
+    {
+      name: 'numberParams',
+      type: 'number',
+      array: true,
+    },
+    {
+      name: 'triggerParams',
+      type: 'json',
+      schema: [
+        {
+          name: 'type',
+          type: 'number',
+          default: 0,
+          description: 'trigger type',
+        },
+        {
+          name: 'booleanParams',
+          type: 'boolean',
+          array: true,
+        },
+        {
+          name: 'stringParams',
+          type: 'string',
+          array: true,
+        },
+        {
+          name: 'numberParams',
+          type: 'number',
+          array: true,
+        },
+        {
+          name: 'actionParams',
+          type: 'json',
+          schema: [
+            {
+              name: 'type',
+              type: 'number',
+              default: 0,
+              description: 'action type',
+            },
+            {
+              name: 'booleanParams',
+              type: 'boolean',
+              array: true,
+            },
+            {
+              name: 'stringParams',
+              type: 'string',
+              array: true,
+            },
+            {
+              name: 'numberParams',
+              type: 'number',
+              array: true,
+            },
+          ],
+          array: true,
+        },
+      ],
+      array: true,
+    },
+    {
+      name: 'actionParams',
+      type: 'json',
+      schema: [
+        {
+          name: 'type',
+          type: 'number',
+          default: 0,
+          description: 'action type',
+        },
+        {
+          name: 'booleanParams',
+          type: 'boolean',
+          array: true,
+        },
+        {
+          name: 'stringParams',
+          type: 'string',
+          array: true,
+        },
+        {
+          name: 'numberParams',
+          type: 'number',
+          array: true,
+        },
+      ],
+      array: true,
+    },
+  ],
+};
