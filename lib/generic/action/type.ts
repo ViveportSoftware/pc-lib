6688eeba0c2bc90fa27a3a2a9b206a0aa08fc42b
Ahdai Tsai <ahdai_tsai@htc.com>
2023-06-09 16:30:33 +0800

    Add and refactor action types

--- lib/generic/action/type.ts
+++ lib/generic/action/type.ts
@@ -3,7 +3,8 @@ export enum Type {
 
   NotificationCenterPublish,
 
-  AddForceInPhysics,
-
-  PlayAnimation,
-};
+  EntityRigidbodyAddForceInPhysics,
+  EntityPlayAnimation,
+  EntityEnable,
+  EntityDisable,
+}
