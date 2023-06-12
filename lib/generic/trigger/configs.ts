abd6a96c89d731b4b5b20c7eae1c6fc75c8e0115
Ahdai Tsai <ahdai_tsai@htc.com>
2023-06-12 11:17:41 +0800

    Refactor trigger configs

--- lib/generic/trigger/configs.ts
+++ lib/generic/trigger/configs.ts
@@ -2,33 +2,7 @@ export const Configs = {
   type: 'json',
   schema: [
     {
-      name: 'type',
-      type: 'number',
-      default: 0,
-      description: 'trigger type',
-    },
-    {
-      name: 'sync',
-      type: 'boolean',
-      default: false,
-    },
-    {
-      name: 'booleanParams',
-      type: 'boolean',
-      array: true,
-    },
-    {
-      name: 'stringParams',
-      type: 'string',
-      array: true,
-    },
-    {
-      name: 'numberParams',
-      type: 'number',
-      array: true,
-    },
-    {
-      name: 'triggerParams',
+      name: 'triggers',
       type: 'json',
       schema: [
         {
@@ -36,6 +10,11 @@ export const Configs = {
           type: 'number',
           default: 0,
           description: 'trigger type',
+        },
+        {
+          name: 'sync',
+          type: 'boolean',
+          default: false,
         },
         {
           name: 'booleanParams',
@@ -50,6 +29,62 @@ export const Configs = {
         {
           name: 'numberParams',
           type: 'number',
+          array: true,
+        },
+        {
+          name: 'triggerParams',
+          type: 'json',
+          schema: [
+            {
+              name: 'type',
+              type: 'number',
+              default: 0,
+              description: 'trigger type',
+            },
+            {
+              name: 'booleanParams',
+              type: 'boolean',
+              array: true,
+            },
+            {
+              name: 'stringParams',
+              type: 'string',
+              array: true,
+            },
+            {
+              name: 'numberParams',
+              type: 'number',
+              array: true,
+            },
+            {
+              name: 'actionParams',
+              type: 'json',
+              schema: [
+                {
+                  name: 'type',
+                  type: 'number',
+                  default: 0,
+                  description: 'action type',
+                },
+                {
+                  name: 'booleanParams',
+                  type: 'boolean',
+                  array: true,
+                },
+                {
+                  name: 'stringParams',
+                  type: 'string',
+                  array: true,
+                },
+                {
+                  name: 'numberParams',
+                  type: 'number',
+                  array: true,
+                },
+              ],
+              array: true,
+            },
+          ],
           array: true,
         },
         {
@@ -78,34 +113,6 @@ export const Configs = {
               array: true,
             },
           ],
-          array: true,
-        },
-      ],
-      array: true,
-    },
-    {
-      name: 'actionParams',
-      type: 'json',
-      schema: [
-        {
-          name: 'type',
-          type: 'number',
-          default: 0,
-          description: 'action type',
-        },
-        {
-          name: 'booleanParams',
-          type: 'boolean',
-          array: true,
-        },
-        {
-          name: 'stringParams',
-          type: 'string',
-          array: true,
-        },
-        {
-          name: 'numberParams',
-          type: 'number',
           array: true,
         },
       ],
