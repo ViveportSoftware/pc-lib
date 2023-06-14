9c317bf4d3ac0ed7385f7b1b37b3550b3d577f15
Ahdai Tsai <ahdai_tsai@htc.com>
2023-06-14 16:33:17 +0800

    Add params comments for trigger and action type

--- lib/plugin/trigger-and-action/action/type.ts
+++ lib/plugin/trigger-and-action/action/type.ts
@@ -1,11 +1,48 @@
 export enum Type {
   Echo,
 
+  /********************************
+   * NotificationCenter
+   ********************************/
+  /**
+   * NotificationCenterPublish
+   *
+   * stringParams[0]: notification name to publish
+   */
   NotificationCenterPublish,
 
+  /********************************
+   * Entity
+   ********************************/
+  /**
+   * EntityRigidbodyAddForceInPhysics
+   *
+   * numberParams[0]: x force
+   * numberParams[1]: y force
+   * numberParams[2]: z force
+   */
   EntityRigidbodyAddForceInPhysics,
+
+  /**
+   * EntityPlayAnimation
+   *
+   * stringParams[0]: animate state to play
+   * booleanParams[1]: is transition
+   */
   EntityPlayAnimation,
+
+  /**
+   * EntityEnable
+   */
   EntityEnable,
+
+  /**
+   * EntityDisable
+   */
   EntityDisable,
+
+  /**
+   * EntityToggleEnabled
+   */
   EntityToggleEnabled,
 }
--- lib/plugin/trigger-and-action/trigger/type.ts
+++ lib/plugin/trigger-and-action/trigger/type.ts
@@ -1,14 +1,58 @@
 export enum Type {
   Echo,
 
+  /********************************
+   * NotificationCenter
+   ********************************/
+  /**
+   * NotificationCenterSubscribe
+   *
+   * stringParams[0]: notification name to subscribe
+   */
   NotificationCenterSubscribe,
+
+  /**
+   * NotificationCenterSubscribe
+   */
   NotificationCenterSubscribeEntityPicking,
 
+  /********************************
+   * Entity
+   ********************************/
+  /**
+   * EntitySubscribeAnimationEvent
+   *
+   * stringParams[0]: animation event name
+   */
   EntitySubscribeAnimationEvent,
+
+  /**
+   * EntitySubscribeAnimationStart
+   */
   EntitySubscribeAnimationStart,
+
+  /**
+   * EntitySubscribeAnimationEnd
+   */
   EntitySubscribeAnimationEnd,
+
+  /**
+   * EntitySubscribeCollisionStart
+   */
   EntitySubscribeCollisionStart,
+
+  /**
+   * EntitySubscribeCollisionEnd
+   */
   EntitySubscribeCollisionEnd,
+
+  /**
+   * EntitySubscribeTriggerEnter
+   */
   EntitySubscribeTriggerEnter,
+
+  /**
+   * EntitySubscribeTriggerLeave
+   */
   EntitySubscribeTriggerLeave,
 }
