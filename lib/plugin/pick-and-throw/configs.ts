ae5a845d5383a7e7df4b6132d23b5c4db5d85968
Ahdai Tsai <ahdai_tsai@htc.com>
2023-07-26 10:23:59 +0800

    Update plugin pick and throw

--- lib/plugin/pick-and-throw/configs.ts
+++ lib/plugin/pick-and-throw/configs.ts
@@ -2,10 +2,16 @@ export const Configs = {
   type: 'json',
   schema: [
     {
-      name: 'throwingForce',
-      type: 'number',
-      default: 10,
-      desc: 'throwing force',
+      name: 'pickable',
+      type: 'json',
+      schema: [
+        {
+          name: 'throwingForce',
+          type: 'number',
+          default: 10,
+          desc: 'throwing force',
+        },
+      ],
     },
   ],
 };
