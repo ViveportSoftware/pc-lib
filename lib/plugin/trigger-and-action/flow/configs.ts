8ccb64a0db5db5cf7fc02ca267612a1e97015fa3
Ahdai Tsai <ahdai_tsai@htc.com>
2023-09-04 15:59:18 +0800

    Add flow control to trigger and action

---
+++ lib/plugin/trigger-and-action/flow/configs.ts
@@ -0,0 +1,56 @@
+export const Configs = {
+  type: 'json',
+  schema: [
+    {
+      name: 'flows',
+      type: 'json',
+      schema: [
+        {
+          name: 'type',
+          type: 'number',
+          default: 0,
+          description: 'flow type',
+        },
+        {
+          name: 'triggerIndexes',
+          type: 'number',
+          array: true,
+        },
+        {
+          name: 'actionParams',
+          type: 'json',
+          schema: [
+            {
+              name: 'type',
+              type: 'number',
+              default: 0,
+              description: 'action type',
+            },
+            {
+              name: 'delay',
+              type: 'number',
+              default: 0,
+            },
+            {
+              name: 'booleanParams',
+              type: 'boolean',
+              array: true,
+            },
+            {
+              name: 'stringParams',
+              type: 'string',
+              array: true,
+            },
+            {
+              name: 'numberParams',
+              type: 'number',
+              array: true,
+            },
+          ],
+          array: true,
+        },
+      ],
+      array: true,
+    },
+  ],
+};
