79dca67067afb35e0d258423dfe8521cc5a24818
Elise_Tung <elise_tung@htc.com>
2023-11-07 10:17:42 +0800

    test: add addMediaPluginType

--- lib/index.ts
+++ lib/index.ts
@@ -4,3 +4,4 @@ export * as InworldNpc from './plugin/inworld-npc';
 export * as AddMedia from './plugin/add-media';
 export {Type as PluginType} from './plugin/type';
 export {Type as TriggerAndActionPluginType} from './plugin/trigger-and-action/type';
+export {Type as AddMediaPluginType} from './plugin/add-media/type';
