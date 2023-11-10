897d5d34c3c77bd79cf6fda3c864a180b377e172
AlbertKeHTC <albert_ke@htc.com>
2023-11-10 17:46:14 +0800

    fix: fixed media configs schema

--- lib/plugin/media/configs.ts
+++ lib/plugin/media/configs.ts
@@ -2,39 +2,45 @@ export const Configs = {
   type: 'json',
   schema: [
     {
-      name: 'assetId',
-      type: 'number',
-      default: 0,
-    },
-    {
-      name: 'billboard',
-      type: 'boolean',
-      default: false,
-    },
-    {
-      name: 'linkHref',
-      type: 'string',
-      default: '',
-    },
-    {
-      name: 'linkButtonText',
-      type: 'string',
-      default: '',
-    },
-    {
-      name: 'controls',
-      type: 'boolean',
-      default: false,
-    },
-    {
-      name: 'autoPlay',
-      type: 'boolean',
-      default: false,
-    },
-    {
-      name: 'loop',
-      type: 'boolean',
-      default: false,
+      name: 'media',
+      type: 'json',
+      schema: [
+        {
+          name: 'assetId',
+          type: 'number',
+          default: 0,
+        },
+        {
+          name: 'billboard',
+          type: 'boolean',
+          default: false,
+        },
+        {
+          name: 'linkHref',
+          type: 'string',
+          default: '',
+        },
+        {
+          name: 'linkButtonText',
+          type: 'string',
+          default: '',
+        },
+        {
+          name: 'controls',
+          type: 'boolean',
+          default: false,
+        },
+        {
+          name: 'autoPlay',
+          type: 'boolean',
+          default: false,
+        },
+        {
+          name: 'loop',
+          type: 'boolean',
+          default: false,
+        },
+      ],
     },
   ],
 };
