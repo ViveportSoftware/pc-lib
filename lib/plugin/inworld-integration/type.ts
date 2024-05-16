a8bede6f8131c89bb8b85c088b2337d1c7a0cb2b
Ahdai Tsai <ahdai_tsai@htc.com>
2024-05-16 11:11:22 +1000

    feat: add plugin inworld integration

---
+++ lib/plugin/inworld-integration/type.ts
@@ -0,0 +1,15 @@
+export enum Type {
+  Base = 0,
+}
+
+export enum AutoFollowMode {
+  None = 1,
+  PathFinding = 2,
+  PhaseWall = 3,
+}
+
+export enum GreetingMode {
+  None = 1,
+  Instruction = 2,
+  Verbatim = 3,
+}
