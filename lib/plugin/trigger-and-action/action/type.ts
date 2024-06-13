891533837b5b3f80b1fcf58b77069a702b2b5791
john👽cheng <john_cheng@htc.com>
2024-06-13 14:57:48 +0800

    feat(external-creator): add external creator enum

--- lib/plugin/trigger-and-action/action/type.ts
+++ lib/plugin/trigger-and-action/action/type.ts
@@ -55,3 +55,40 @@ export enum InworldTriggerMode {
   SayVerbatim = 2,
   TriggerName = 3,
 }
+
+export enum ExternalCreatorType {
+  Base = 0,
+
+  /********************************
+   * NotificationCenter
+   ********************************/
+  NotificationCenterPublish = 200001,
+
+  /********************************
+   * Entity
+   ********************************/
+  EntityRigidbodyAddForceInPhysics = 300001,
+  EntityPlayAnimation = 300002,
+  EntityEnable = 300003,
+  EntityDisable = 300004,
+  EntityToggleEnabled = 300005,
+  EntityFadeIn = 300006,
+  EntityFadeOut = 300007,
+  EntityPlaySound = 300008,
+  EntityEnableCollision = 300009,
+  EntityDisableCollision = 300010,
+  EntityToggleCollision = 300011,
+  EntityEnableByTag = 300014,
+  EntityDisableByTag = 300015,
+  EntityCheckPoint = 300016,
+  EntityEnableById = 300017,
+  EntityDisableById = 300018,
+  EntityStopSound = 3000019,
+
+  /********************************
+   * Player related
+   ********************************/
+  Quest = 400001,
+  TeleportAvatar = 400002,
+  Vote = 400004,
+}
--- lib/plugin/trigger-and-action/trigger/type.ts
+++ lib/plugin/trigger-and-action/trigger/type.ts
@@ -30,3 +30,24 @@ export enum Type {
   EntitySubscribeTriggerEnter = 300006,
   EntitySubscribeTriggerLeave = 300007,
 }
+
+export enum ExternalCreatorType {
+  Base = 0,
+
+  /********************************
+   * NotificationCenter
+   ********************************/
+  NotificationCenterSubscribe = 200001,
+  NotificationCenterSubscribeEntityPicking = 200002,
+
+  /********************************
+   * Entity
+   ********************************/
+  EntitySubscribeAnimationEvent = 300001,
+  EntitySubscribeAnimationStart = 300002,
+  EntitySubscribeAnimationEnd = 300003,
+  EntitySubscribeCollisionStart = 300004,
+  EntitySubscribeCollisionEnd = 300005,
+  EntitySubscribeTriggerEnter = 300006,
+  EntitySubscribeTriggerLeave = 300007,
+}
