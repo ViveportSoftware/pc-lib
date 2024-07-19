7c9ffb7295aa0ee6d6a92f801acae658f292c50f
spencer-Ahfu <spencer_yang@htc.com>
2024-07-19 10:20:27 +0800

    feat: add performer area setting in custom rigidbody module

--- lib/plugin/physic/desc.ts
+++ lib/plugin/physic/desc.ts
@@ -6,5 +6,17 @@ export const Desc = {
       default: false,
       desc: 'ignored by camera auto-zoom',
     },
+    isCollideOwner: {
+      default: true,
+      desc: 'collide with the room owner',
+    },
+    isCollideVisitor: {
+      default: true,
+      desc: 'collide with the visitor',
+    },
+    isCollideCursor: {
+      default: true,
+      desc: 'click through the rigidbody',
+    },
   },
 };
