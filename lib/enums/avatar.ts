5e79ee30dbb34ffb6d547934c5f45896da981a99
YuniChou <yuni_chou@htc.com>
2024-11-06 15:06:26 +0800

    feat: export avatar related enum

---
+++ lib/enums/avatar.ts
@@ -0,0 +1,15 @@
+export enum AvatarGenderType {
+  Female = 0,
+  Male = 1,
+}
+
+export enum AvatarDataType {
+  Unknown = -1,
+  Stylized = 0,
+  Realistic = 1,
+  VRM = 4,
+  GLB = 6,
+  Cartoon = 7,
+  RealPerson = 8,
+  CartoonNew = 9,
+}
--- lib/index.ts
+++ lib/index.ts
@@ -7,6 +7,7 @@ export * as Quest from './plugin/quest';
 export * as GPUMeshInstancing from './plugin/gpu-mesh-instancing';
 export * as Networked from './plugin/networked';
 export * as SceneSettings from './enums/scene-settings';
+export * as Avatar from './enums/avatar';
 export * as AnimatePath from './plugin/animate-path';
 export * as InworldIntegration from './plugin/inworld-integration';
 export * as Seat from './plugin/seat';
