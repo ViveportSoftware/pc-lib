ec5b2b661ba9466ddef90eb5ceb2c368dfc04f62
Spencer Lin <spencer_lin@htc.com>
2024-11-07 12:09:17 +0800

    feat(create-extension-sdk): revamp codebase

--- sdk/create-extensions-sdk/index.ts
+++ sdk/create-extensions-sdk/index.ts
@@ -1,85 +1,50 @@
 import * as TriggerAndAction from '../../lib/plugin/trigger-and-action';
 import * as Avatar from '../../lib/enums/avatar';
 
+function enumToNumberRecord<T extends Record<string, any>>(enumObj: T): Record<string, number> {
+  return Object.entries(enumObj)
+    .filter((entry): entry is [string, number] => typeof entry[1] === 'number')
+    .reduce(
+      (acc, [key, value]) => {
+        acc[key] = value;
+        return acc;
+      },
+      {} as Record<string, number>
+    );
+}
+
 export default class CreateExtensionsSDK {
   private TriggerType: Record<string, number>;
   private ActionType: Record<string, number>;
+  private AvatarGenderType: Record<string, number>;
   prefix: Record<string, string>;
 
   constructor() {
-    this.TriggerType = Object.entries(TriggerAndAction.Trigger.Type)
-      .filter(
-        (entry): entry is [string, number] => typeof entry[1] === 'number'
-      )
-      .reduce(
-        (acc, [key, value]) => {
-          acc[key] = value;
-          return acc;
-        },
-        {} as Record<string, number>
-      );
-
-    this.ActionType = Object.entries(TriggerAndAction.Action.Type)
-      .filter(
-        (entry): entry is [string, number] => typeof entry[1] === 'number'
-      )
-      .reduce(
-        (acc, [key, value]) => {
-          acc[key] = value;
-          return acc;
-        },
-        {} as Record<string, number>
-      );
+    this.TriggerType = enumToNumberRecord(TriggerAndAction.Trigger.Type)
+    this.ActionType = enumToNumberRecord(TriggerAndAction.Action.Type)
+    this.AvatarGenderType = enumToNumberRecord(Avatar.AvatarGenderType)
 
     this.prefix = {
       TriggerType: 'trigger:',
       ActionType: 'action:',
     };
-
-    this.generateGetters();
-  }
-
-  private generateGetters() {
-    Object.keys(this.TriggerType).forEach(key => {
-      Object.defineProperty(this, `getTrigger${key}`, {
-        get: () => `${this.prefix['TriggerType'] + this.TriggerType[key]}`,
-      });
-    });
-
-    Object.keys(this.ActionType).forEach(key => {
-      Object.defineProperty(this, `getAction${key}`, {
-        get: () => `${this.prefix['ActionType'] + this.ActionType[key]}`,
-      });
-    });
   }
 
   toTemplateLiteral() {
-    const AvatarGenderType = Object.entries(Avatar.AvatarGenderType)
-      .filter(
-        (entry): entry is [string, number] => typeof entry[1] === 'number'
-      )
-      .reduce(
-        (acc, [key, value]) => {
-          acc[key] = value;
-          return acc;
-        },
-        {} as Record<string, number>
-      );
-
     return `export const TriggerTypes = {
-  ${Object.keys(this.TriggerType)
-    .map(key => `${key}: '${(this as any)[`getTrigger${key}`]}'`)
+  ${Object.entries(this.TriggerType)
+    .map(([key, value]) => `${key}: '${this.prefix['TriggerType'] + value}'`)
     .join(',\n  ')}
 }
 
 export const ActionTypes = {
-  ${Object.keys(this.ActionType)
-    .map(key => `${key}: '${(this as any)[`getAction${key}`]}'`)
+  ${Object.entries(this.ActionType)
+    .map(([key, value]) => `${key}: '${this.prefix['ActionType'] + value}'`)
     .join(',\n  ')}
 }
 
 export const AvatarGenderType = {
-  ${Object.entries(AvatarGenderType)
+  ${Object.entries(this.AvatarGenderType)
     .map(([key, value]) => `${key}: ${value}`)
     .join(',\n  ')}
 }
