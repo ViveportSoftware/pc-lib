0226b0d1e932ec7ef4a8b7e0766e31bc1c37ea35
Steven.YA Chen(陳怡安) <Steven.YA_Chen@htc.com>
2024-11-20 17:46:08 +0800

    feat(create-sdk):  PlayerFlyModeType -> EnvironmentFlyModeType

--- lib/index.ts
+++ lib/index.ts
@@ -8,7 +8,7 @@ export * as GPUMeshInstancing from './plugin/gpu-mesh-instancing';
 export * as Networked from './plugin/networked';
 export * as SceneSettings from './enums/scene-settings';
 export * as Avatar from './enums/avatar';
-export * as Player from './enums/player';
+export * as Environment from './enums/environment';
 export * as AnimatePath from './plugin/animate-path';
 export * as InworldIntegration from './plugin/inworld-integration';
 export * as Seat from './plugin/seat';
