530d2f7f2e9add2509f057bcb9717f8f5728393c
YuniChou <yuni_chou@htc.com>
2024-12-10 16:41:19 +0800

    feat(create-SDK): add remotePlayer joined & left event

--- sdk/create-sdk-ts/services/player/interface/IPlayerService.ts
+++ sdk/create-sdk-ts/services/player/interface/IPlayerService.ts
@@ -1,10 +1,11 @@
 import {ILocalPlayer} from './ILocalPlayer';
 import {IRemotePlayer} from './IRemotePlayer';
+import {EventHandlerMethods} from '../../../types';
 
 /**
  * Define PlayerService interface.
  */
-export interface IPlayerService {
+export interface IPlayerService extends EventHandlerMethods {
   /**
    * Get local player information.
    */
@@ -19,4 +20,38 @@ export interface IPlayerService {
    * Get the number of players currently connected in the room.
    */
   readonly playerCount: number;
+
+  /**
+   * 訂閱指定的事件
+   * @param event - 事件名稱
+   * @param listener - 回調函數
+   */
+  on<T extends keyof IPlayerServiceEvents>(
+    event: T,
+    listener: IPlayerServiceEvents[T]
+  ): pc.EventHandle;
+
+  /**
+   * 取消訂閱指定的事件
+   * @param event - 事件名稱
+   * @param listener - 回調函數
+   */
+  off<T extends keyof IPlayerServiceEvents>(
+    event: T,
+    listener: IPlayerServiceEvents[T]
+  ): pc.EventHandler;
+}
+
+export interface IPlayerServiceEvents {
+  /**
+   * 當 Remote Player 加入 session 時觸發
+   * @param remotePlayer - 遠端玩家的實例，該物件為 `IRemotePlayer` 類別，包含玩家的相關資訊。
+   */
+  'remotePlayer:joined': (remotePlayer: IRemotePlayer) => void;
+
+  /**
+   * 當 Remote Player 離開 session 時觸發
+   * @param remotePlayer - 遠端玩家的實例，該物件為 `IRemotePlayer` 類別，包含玩家的相關資訊。
+   */
+  'remotePlayer:left': (remotePlayer: IRemotePlayer) => void;
 }
