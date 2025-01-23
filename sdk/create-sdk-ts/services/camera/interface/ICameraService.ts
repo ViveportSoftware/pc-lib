4b05d1fdf4cc6379cfd255ba3e6a1ac64e674abe
Steven.YA Chen(陳怡安) <Steven.YA_Chen@htc.com>
2025-01-23 15:41:36 +0800

    feat(create-sdk): rename isLockCameraZoomDistance  -> canZoom

--- sdk/create-sdk-ts/services/camera/interface/ICameraService.ts
+++ sdk/create-sdk-ts/services/camera/interface/ICameraService.ts
@@ -17,7 +17,7 @@ export interface ICameraService {
   /**
    * Control whether the VIVERSE CREATE Camera can zoom or not.
    */
-  isLockCameraZoomDistance: boolean;
+  canZoom: boolean;
 
   /**
    * @beta Control the minimum distance between the VIVERSE CREATE third-person camera and the player.
