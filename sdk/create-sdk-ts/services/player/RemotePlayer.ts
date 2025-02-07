c7b47ac209df0237b7cd73a180e56efc1ae9018c
Albert Ke <albert_ke@htc.com>
2025-02-07 18:00:05 +0800

    feat(create-sdk): add scale-avatar sdk

--- sdk/create-sdk-ts/services/player/RemotePlayer.ts
+++ sdk/create-sdk-ts/services/player/RemotePlayer.ts
@@ -19,6 +19,8 @@ class RemotePlayer extends Player implements IRemotePlayer {
     return false;
   }
 
+  scaleAvatar(scale: number): void {}
+
   on<T extends keyof IPlayerEvents>(
     event: T,
     listener: IPlayerEvents[T]
