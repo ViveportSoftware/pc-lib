d1a3f687714fb886404c710e0f297c0da287a000
IjonHsieh <ijon_hsieh@htc.com>
2025-04-18 16:27:56 +0800

    docs: add private tag for planned

--- sdk/create-sdk-ts/services/player/interface/IRemotePlayer.ts
+++ sdk/create-sdk-ts/services/player/interface/IRemotePlayer.ts
@@ -5,16 +5,19 @@ import {IPlayer} from './IPlayer';
  */
 export interface IRemotePlayer extends IPlayer {
   /**
+   * @private
    * @planned Whether the player is admin.
    */
   readonly isAdmin: boolean;
 
   /**
+   * @private
    * @planned Whether the player is owner.
    */
   readonly isOwner: boolean;
 
   /**
+   * @private
    * @planned Whether the player is talking.
    */
   readonly isTalking: boolean;
--- sdk/create-sdk-ts/services/xr/enums/xr.ts
+++ sdk/create-sdk-ts/services/xr/enums/xr.ts
@@ -1,6 +1,7 @@
 export enum SessionTypes {
   Vr = 0,
   /**
+   * @internal
    * @planned VIVERSE AR support is currently unavailable.
    */
   Ar = 1,
