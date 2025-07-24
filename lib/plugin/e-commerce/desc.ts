18b717b41ea1610be928ae139b1ecae0203f451e
winny tu <winny_tu@htc.com>
2025-07-24 11:57:28 +0900

    feat: [3DEC] add e-commerce plugin setting

---
+++ lib/plugin/e-commerce/desc.ts
@@ -0,0 +1,12 @@
+import {Type} from './type';
+
+export const Desc = {
+  [Type.Camera]: {
+    rotateY: {
+      desc: 'rotate Y +/- max degree',
+    },
+    rotateX: {
+      desc: 'rotate X +/- max degree',
+    },
+  },
+};
